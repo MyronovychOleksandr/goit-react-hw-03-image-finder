68a65c3f9617cb2145710902ff6b3b0ca3028cdc
MyronovychOleksandr <muronalex@ukr.net>
2021-01-15 16:02:25 +0200

    axios

--- src/services/imagesApi.js
+++ src/services/imagesApi.js
@@ -1,28 +1,38 @@
 import axios from "axios";
 
-const BASE_URL = "https://pixabay.com/api";
-const API_KEY = "18864645-939ea59db410a3043a30cf718";
+const apiKey = "18864645-939ea59db410a3043a30cf718";
 
-axios.defaults.baseURL = BASE_URL;
-axios.defaults.params = {
-  key: API_KEY,
-  image_type: "photo",
-  orientation: "horizontal",
-  per_page: 12,
-};
-
-const fetchImagesWithQuery = async (q, page) => {
-  try {
-    const { data } = await axios.get("", {
-      params: { q, page },
-    });
-    return data.hits;
-  } catch (error) {
-    console.log("error", { error });
-    return [];
-  }
+const fetchImagesWithQuery = (searchQuery, page) => {
+  return axios
+    .get(
+      `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+    .then((response) => response.data.hits);
 };
 
 export default {
   fetchImagesWithQuery,
 };
+
+// const BASE_URL = "https://pixabay.com/api";
+// const API_KEY = "18864645-939ea59db410a3043a30cf718";
+
+// axios.defaults.baseURL = BASE_URL;
+// axios.defaults.params = {
+//   key: API_KEY,
+//   image_type: "photo",
+//   orientation: "horizontal",
+//   per_page: 12,
+// };
+
+// const fetchImagesWithQuery = async (q, page) => {
+//   try {
+//     const { data } = await axios.get("", {
+//       params: { q, page },
+//     });
+//     return data.hits;
+//   } catch (error) {
+//     console.log("error", { error });
+//     return [];
+//   }
+// };
